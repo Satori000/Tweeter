fd60757d7621c7c5d8508794c8e22c096321450d
Satori <shakeebmajid@ufl.edu>
2016-02-08 00:01:57 -0500

    all major functionality except favorite count

---
+++ TwitterClient/TweetCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetCell.swift
+//  TwitterClient
+//
+//  Created by Shakeeb Majid on 2/7/16.
+//  Copyright © 2016 Shakeeb Majid. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
